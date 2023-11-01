8d43b4c26f05b21806a0a22efa458ae636dad054
spelzi <60978130+spelzi@users.noreply.github.com>
2023-11-01 16:44:42 +0100

    web page

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
 // server.js
 const http = require("http");
-const App = require("./App");
+const app = require("./app");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello Node!!!!</h1>\n");
-});
+const server = http.createServer(app);
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
